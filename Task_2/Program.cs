01e7ba10d39e7b0914813cd14bbcd4d4573e09cb
Ilya Yurchenkov <yurchenkov.99@gmail.com>
2022-08-29 23:40:17 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Clear();
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+FillArray(numbers);
+PrintArray(numbers);
+
+int numbersString = ReadInt("Введите номер строки: ");
+int numbersColumn = ReadInt("Введите номер столбца: ");
+
+if(numbersString < numbers.GetLength(0) && numbersColumn < numbers.GetLength(1))
+{ 
+    Console.WriteLine(numbers[numbersString,numbersColumn]);
+}
+else
+{
+Console.WriteLine("Такой позиции нет в массиве: ");
+}
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
